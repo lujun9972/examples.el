417ef56c983979ad0ac158de5e8947cd4aa8b104
lujun9972 <lujun9972@gmail.com>
2016-08-03 22:30:28 +0800

    重构

--- examples.el
+++ examples.el
@@ -26,15 +26,21 @@ The cdr of element is the src-block language list"
       (cdr (assoc major-mode examples-mode-lang-alist))
     (list (replace-regexp-in-string "-mode$" "" (symbol-name major-mode)))))
 
-(defun examples-get-headline-and-src-blocks (languages)
+(defun examples-get-headline-and-src-blocks (languages &rest example-org-files)
   ""
-  (let ((headline-and-blocks (org-element-map (org-element-parse-buffer) 'headline
-                                (lambda (ele)
-                                  (cons (org-element-property :raw-value ele)
-                                        (examples-get-src-blocks ele languages))))))
-    (cl-delete-if (lambda (x)
-                    (null (cdr x)))
-                  headline-and-blocks)))
+  (let ((example-org-files (or example-org-files examples-default-org-files)))
+    (with-temp-buffer
+      (mapc (lambda (file)
+              (insert-file-contents file)
+              (newline))
+            example-org-files) 
+      (let ((headline-and-blocks (org-element-map (org-element-parse-buffer) 'headline
+                                   (lambda (ele)
+                                     (cons (org-element-property :raw-value ele)
+                                           (examples-get-src-blocks ele languages))))))
+        (cl-delete-if (lambda (x)
+                        (null (cdr x)))
+                      headline-and-blocks)))))
 
 (defun examples-get-src-blocks (element languages)
   (org-element-map element 'src-block
@@ -54,25 +60,19 @@ The cdr of element is the src-block language list"
   (interactive)
   (unless yas-minor-mode
     (yas-minor-mode 1))
-  (let ((example-org-files (or example-org-files examples-default-org-files))
-        (languages (examples-get-response-languages-by-major-mode major-mode)))
-    (let* ((headline-and-blocks (with-temp-buffer
-                        (mapc (lambda (file)
-                                (insert-file-contents file)
-                                (newline))
-                              example-org-files) 
-                        (examples-get-headline-and-src-blocks languages)))
-           (headlines (mapcar #'car headline-and-blocks))
-           (headline (completing-read "example:" headlines))
-           (src-blocks (cdr (assoc-string headline headline-and-blocks)))
-           (src-block (if (> (length src-blocks) 1)
-                          (completing-read "" src-blocks)
-                        (or (car src-blocks) "")))
-           (idx 0))
-      (yas-expand-snippet (replace-regexp-in-string "{[^{}0-9][^{}]*}"
-                                                    (lambda (text)
-                                                      (let ((desc (substring text 1 (- (length text) 1))))
-                                                        (setq idx (+ 1 idx))
-                                                        (format "{%d:%s}" idx desc)))
-                                                    src-block)))))
+  (let* ((languages (examples-get-response-languages-by-major-mode major-mode))
+         (headline-and-blocks (examples-get-headline-and-src-blocks languages))
+         (headlines (mapcar #'car headline-and-blocks))
+         (headline (completing-read "example:" headlines))
+         (src-blocks (cdr (assoc-string headline headline-and-blocks)))
+         (src-block (if (> (length src-blocks) 1)
+                        (completing-read "" src-blocks)
+                      (or (car src-blocks) "")))
+         (idx 0))
+    (yas-expand-snippet (replace-regexp-in-string "{[^{}0-9][^{}]*}"
+                                                  (lambda (text)
+                                                    (let ((desc (substring text 1 (- (length text) 1))))
+                                                      (setq idx (+ 1 idx))
+                                                      (format "{%d:%s}" idx desc)))
+                                                  src-block))))
 
